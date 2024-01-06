156ad4e3a3ed47335b6b29db6214be7f9943c8b8
Jörg Dölfer <doelfer@googlemail.com>
2024-01-06 23:34:43 +0100

    hello world with tests

--- main.go
+++ main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-   fmt.Println("Hello world")
+    greeting := greet("en")
+    fmt.Println(greeting)
+}
+
+// language represents the language’s code
+type language string
+
+// phrasebook holds greeting for each supported language
+var phrasebook = map[language]string{
+    "el": "Χαίρετε Κόσμε",     // Greek
+    "en": "Hello world",       // English
+    "fr": "Bonjour le monde",  // French
+    "de": "Hallo Welt",        // German
+    "he": "שלום עולם",         // Hebrew
+    "ur": "ہیلو دنیا",         // Urdu
+    "vi": "Xin chào Thế Giới", // Vietnamese
+}
+
+// greet says hello to the world in various languages
+func greet(l language) string {
+    greeting, ok := phrasebook[l]
+    if !ok {
+        return fmt.Sprintf("unsupported language: %q", l)
+    }
+
+    return greeting
 }
