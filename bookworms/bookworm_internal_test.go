ef5802cf0f25ba7d82ed6264b22cac924648ed9b
Jörg Dölfer <doelfer@googlemail.com>
2024-01-07 18:03:51 +0100

    bookworm added test

---
+++ bookworms/bookworm_internal_test.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"testing"
+)
+
+var (
+	handmaidsTale = Book{Author: "Margaret Atwood", Title: "The Handmaid's Tale"}
+	oryxAndCrake  = Book{Author: "Margaret Atwood", Title: "Oryx and Crake"}
+	theBellJar    = Book{Author: "Sylvia Plath", Title: "The Bell Jar"}
+	janeEyre      = Book{Author: "Charlotte Brontë", Title: "Jane Eyre"}
+)
+
+func TestLoadBookworms_Success(t *testing.T) {
+	type testCase struct {
+		bookwormsFile string
+		want          []Bookworm
+		wantErr       bool
+	}
+	tests := map[string]testCase{
+		"file exists": {
+			bookwormsFile: "testdata/bookworms.json",
+			want: []Bookworm{
+				{Name: "Fadi", Books: []Book{handmaidsTale, theBellJar}},
+				{Name: "Peggy", Books: []Book{oryxAndCrake, handmaidsTale, janeEyre}},
+			},
+			wantErr: false,
+		},
+		"file doesn't exist": {
+			bookwormsFile: "testdata/no_file_here.json",
+			want:          nil,
+			wantErr:       true,
+		},
+		"invalid JSON": {
+			bookwormsFile: "testdata/invalid.json",
+			want:          nil,
+			wantErr:       true,
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := loadBookworms(tc.bookwormsFile)
+
+			if tc.wantErr {
+				if err == nil {
+					t.Fatal("expected err, got nothing")
+				}
+				return
+			}
+
+			// we aren't expecting errors here, this should be the happy path
+			if err != nil {
+				t.Fatalf("expected no error, got %v", err)
+			}
+			if !equalBookworms(t, got, tc.want) {
+				t.Fatalf("different result: got %v, expected %v", got, tc.want)
+			}
+		})
+	}
+}
+
+// equalBookworms is a helper to test the equality of two lists of Bookworms.
+func equalBookworms(t *testing.T, bookworms, target []Bookworm) bool {
+	t.Helper()
+
+	if len(bookworms) != len(target) {
+		// Early exit!
+		return false
+	}
+
+	for i := range bookworms {
+		// Verify the name of the Bookworm.
+		if bookworms[i].Name != target[i].Name {
+			return false
+		}
+		// Verify the content of the collections of Books for each Bookworm.
+		if !equalBooks(t, bookworms[i].Books, target[i].Books) {
+			return false
+		}
+	}
+
+	// Everything is equal!
+	return true
+}
+
+// equalBooks is a helper to test the equality of two lists of Books.
+func equalBooks(t *testing.T, books, target []Book) bool {
+	t.Helper()
+
+	if len(books) != len(target) {
+		// Early exit!
+		return false
+	}
+	// Verify the content of the collections of Books for each Bookworm.
+	for i := range target {
+		if target[i] != books[i] {
+			return false
+		}
+	}
+	// Everything is equal!
+	return true
+}
